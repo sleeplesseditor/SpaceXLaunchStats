12c41e7a36e1847cb9ca27972357b19c26a08dcb
sleeplesseditor <josh@bryson.me.uk>
2018-12-12 00:40:10 +0000

    Styling update and date formatting

--- client/src/components/launches.js
+++ client/src/components/launches.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import LaunchItem from './LaunchItem';
+import MissionKey from './MissionKey';
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
@@ -18,7 +19,8 @@ export class Launches extends Component {
     render() {
       return (
         <Fragment>
-          <h1 className="display-4 my-3">Launches</h1>
+          <h4 className="display-5 my-3">Launches</h4>
+          <MissionKey />
           <Query query={LAUNCHES_QUERY}>
             {
               ({ loading, error, data }) => {
