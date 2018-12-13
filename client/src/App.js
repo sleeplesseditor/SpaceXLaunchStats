3738ca3db511b15f1f470e16cc65bd220008fba9
sleeplesseditor <josh@bryson.me.uk>
2018-12-13 12:39:15 +0000

    Pre-Heroku deploy setup

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Launch from './components/Launch';
 import './App.css';
 
 const client = new ApolloClient({
-  uri: `http://localhost:5000/graphql`
+  uri: `/graphql`
 });
 
 class App extends Component {
---
+++ public/precache-manifest.338d049e437993a84b03b003aa9b9caa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ec7e423cbce40e5c304",
+    "url": "/static/js/main.8ec7e423.chunk.js"
+  },
+  {
+    "revision": "9ef3b7584847e7990496",
+    "url": "/static/js/1.9ef3b758.chunk.js"
+  },
+  {
+    "revision": "8ec7e423cbce40e5c304",
+    "url": "/static/css/main.b1e19ca0.chunk.css"
+  },
+  {
+    "revision": "aea04fe37e7c398ec076160ee48a7b36",
+    "url": "/index.html"
+  }
+];
